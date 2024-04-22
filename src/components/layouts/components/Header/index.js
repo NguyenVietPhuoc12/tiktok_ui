c3a86ac09c246d379ba993f69fea575c908acb71
NguyenVietPhuoc12 <nguyenvietphuoc24.12@gmail.com>
2024-04-22 13:26:08 +0700

    8. Built UI for Header project

--- src/components/layouts/components/Header/index.js
+++ src/components/layouts/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faEllipsisVertical, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +11,28 @@ const Header = () => {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('header__content')}>
-                <h2>Header</h2>
+                <div className={cx('header__content-logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('header__content-search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('header__content-clear-btn')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon
+                        style={{ display: 'none' }}
+                        className={cx('header__content-loading')}
+                        icon={faSpinner}
+                    />
+                    <button className={cx('header__content-search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('header__content-actions')}>
+                    <button>Upload</button>
+                    <button>Log in</button>
+                    <FontAwesomeIcon icon={faEllipsisVertical} />
+                </div>
             </div>
         </header>
     );
